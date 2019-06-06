624f8a777d86dca71d0e20ca832f750da323a229
jpiazza <jdub@quantumleap-recruitment.com>
2019-06-06 16:14:47 -0400

    rebuilt checkDefense(), added Obj func for board

--- App.jsx
+++ App.jsx
@@ -8,10 +8,8 @@ const arr = [
       [1,5,9], [3,5,7] 
       ];
 
-      let monitor = {
-        X : [],
-        O : []
-      }
+let copy = [];
+      
 
 export class App extends Component{
    constructor(props) {
@@ -26,49 +24,63 @@ export class App extends Component{
         unpauseCpu: false,
         playsFirst: true, // firstMove, goesFirst, movesFirst, startsFirst, playerStarts, winnerStarts, winnerMovesFirst, winnersFirst
         preventClicks: false,
-        prevThreat: false,        
+        prevThreat: false, // isn't used      
       }
 
     }
     
     componentDidMount() {
+      // animation used to display player character selection - called in clickHandler() too
       this.startAnimation = () => {
         // starts selectPlayer behind Title 
         this.player = document.getElementById('selectPlayer');
         this.player.style.marginTop = '-3.55em'
-        let num = -3.55;
-        monitor = { X: [], O : [] }
-        
-        Object.keys(monitor).forEach(key => {
-          for(let i = 0; i < 8; i++) {
-            monitor[key].push([]);
-          }
-        });
+        let size = -3.55;
+
         // animation for selecting player icon
         this.startAnimate = setInterval(() => {
-          this.player.style.marginTop = num.toFixed(2) + 'em';  
-          if( num.toFixed(2) >= 0.00) { 
+          this.player.style.marginTop = size.toFixed(2) + 'em';  
+          if( size.toFixed(2) >= 0.00) { 
             this.player.style.zIndex = '1'; 
             return clearInterval(this.startAnimate);     
           }
-          num += .05;    
+          size += .05;    
         }, 8);
       };
       
       this.startAnimation();
       
       // highlights tiles under mouse cursor
-      this.board = document.getElementById('board');
+      this.tile = document.getElementById('board');
   
-      this.board.addEventListener('mouseover', (evt) => {
+      this.tile.addEventListener('mouseover', (evt) => {
         let id = evt.target.id;
         if(Number(id)) evt.target.style.backgroundColor = "#808080";
       })
   
-      this.board.addEventListener('mouseout', (evt) => {
+      this.tile.addEventListener('mouseout', (evt) => {
         let id = evt.target.id;
         if(Number(id)) evt.target.style.backgroundColor = "#666";
       })
+
+      this.board = {};
+
+      this.createBoard = (() => {
+        return {
+          Board : () => {
+            this.board = {
+              played : [],
+              remain : [1,2,3,4,5,6,7,8,9],
+              project: {},
+              data: {}
+            }
+          },
+          Init: function() {
+            this.Board();
+          }
+        }
+      })();
+      this.createBoard.Init(); 
       
     }
 
@@ -78,18 +90,41 @@ export class App extends Component{
   
     componentWillUnmount() {
       clearInterval(this.startAnimate);
-      this.board.removeEventListener('mouseover');
-      this.board.removeEventListener('mouseout');
+      this.tile.removeEventListener('mouseover');
+      this.tile.removeEventListener('mouseout');
     }
-    
+
     // select 'X' or 'O' to start game
     selectPlayer(icon) {
-      icon.preventDefault();      
-
+      icon.preventDefault();
+      
       let click = icon.target.innerHTML;
 
       this.recordTiles = {};
       this.tilesRemain = [1,2,3,4,5,6,7,8,9];
+
+      //this.board = {};
+
+      // this.createBoard = (() => {
+      //   return {
+      //     Board : () => {
+      //       this.board = {
+      //         played : [],
+      //         remain : [1,2,3,4,5,6,7,8,9],
+      //         project: {},
+      //         data: {}
+      //       }
+      //     },
+      //     Init: function() {
+      //       this.Board();
+      //     }
+      //   }
+      // })();
+      // this.createBoard.Init();      
+      //this.createBoard.Init();
+      //console.log(this.board)
+
+      arr.map( item => copy.push(item.slice()));
 
       this.setState({
         player   : click,
@@ -97,18 +132,19 @@ export class App extends Component{
       });
       
       this.removeAnimation = () => {
-        let num = 0;
+        let size = 0;
         this.player.style.zIndex = '-1';
 
         this.endAnimate = setInterval( () => {
-          this.player.style.marginTop = num.toFixed(2) + 'em';
-          if( num.toFixed(2) <= -3.55) return clearInterval(this.endAnimate);
-          num -= .05;
+          this.player.style.marginTop = size.toFixed(2) + 'em';
+          if( size.toFixed(2) <= -3.55) return clearInterval(this.endAnimate);
+          size -= .05;
         }, 8);
       };
 
-      this.removeAnimation();
-
+      this.removeAnimation(); // only place this function is used consider removing
+      
+      // used if player loses game computer will go first
       if(!this.state.playsFirst) {
         //this.setState({preventClicks : true}); // validate importance/necessasity
         setTimeout(() => {
@@ -129,7 +165,7 @@ export class App extends Component{
           this.id = document.getElementById(evt.target.id);
           this.id.innerHTML = player;
           this.state.unpauseCpu = true;
-          this.gameLog(player, event.target.id)
+          this.gameLog(player, parseInt(event.target.id))
         } else if (!player) {
           alert('Select "X" or "O" to start game'); // convert #title header to message board via created error function
         } else {
@@ -139,21 +175,25 @@ export class App extends Component{
       
     }
     
-    // handle cpu and player movements
+    // handle cpu and player movements consider changing to gameCPU
     gameLog(logPlayer, logTileId) {
-      //console.log('gameLog', 'preventClicks',  this.state.preventClicks)
+      // logPlayer = X or O, logTileId = typeof number 1-9
       const resetGame = () => {
+        
         // set tiles to default
         let tiles = document.getElementsByClassName('tile');
-
         for(let i = 0; i < tiles.length; i++) {
           tiles[i].innerHTML = "";
-          //tiles[i].style.cssText += 'color: white; text-shadow: 2px 0 2px lime';
           tiles[i].classList.remove('flash');
         }
+        
+        // empty contents of copied arr
+        copy.splice(0);
+        
+        // activate select player div
+        this.startAnimation(); 
 
-        this.startAnimation(); // allow player to select new icon
-
+        // setState defaults
         this.setState({
           player  : '',
           computer: '',
@@ -163,50 +203,77 @@ export class App extends Component{
         });
       };
 
-      const evaluateGameStatus = () => {
-        console.log('evaluateGameStatus')
-        setTimeout( () => {
-          this.cpuHandler(this.recordTiles, this.tilesRemain);
-        }, 800);
-      };
-
+      // ?? verify player clicks are prevented if computer is going first
       this.setState({
         preventClicks : this.state.preventClicks ? false : true,
       });
            
+      // remove selected tile number and add to recordTiles Object
       this.tilesRemain.splice( this.tilesRemain.indexOf(parseInt(logTileId)), 1);   
       this.recordTiles[logTileId] = logPlayer;
 
+      // acquire new length of remaining tile array
       let playsRemain = this.tilesRemain.length;
-      
-      if(!playsRemain) {       
-        return this.flashTiles(10, () => {
+
+      this.board.data[logTileId] = logPlayer;
+      this.board.played.push(logTileId);
+      this.board.remain.splice(this.board.remain.indexOf(parseInt(logTileId)), 1); 
+
+      // cloned array - replace number with player icon and count for strategy or win
+      for(let a = 0; a < copy.length; a++) {
+        let count = 0, compCount = 0;
+        for(let b = 0; b < copy[a].length; b++) {
+          if(copy[a][b] === logTileId) copy[a].splice(b, 1, logPlayer);
+          if(copy[a][b] === this.state.player) count++;
+          if(copy[a][b] === this.state.computer) compCount++;
+        }
+
+        // alerts a win
+        if(count === 3 || compCount === 3) {
+          return this.flashTiles(arr[a], () => {
+            resetGame();
+          });         
+        }
+        // alerts player to possible win
+        if(count === 2 ) {
+          if(!compCount)  this.board.project['player'] = copy[a];
+        }
+
+        // alerts computer to possible win
+        if(compCount === 2) {
+          if(!count) this.board.project['computer'] = copy[a];
+        }
+
+      }
+      console.log('gameLog', this.board)
+      // alert for no plays left - reset board
+      if(!this.board.remain.length) { 
+        this.createBoard.Init()      
+        return this.flashTiles(this.board.remain, () => {
           resetGame();
         })
       }
-      console.log('playsRemain', playsRemain)
+      
+      // pauses cpu loop - wait for player move
       if(this.state.unpauseCpu) {
-        if(playsRemain <= 4) {
-          evaluateGameStatus();
-        } else {
           setTimeout(() => {
-            this.cpuHandler(this.recordTiles, (9 - playsRemain))
+            this.cpuHandler(this.recordTiles, (9 - this.board.remain.length))
           }, 800);
-        }
       }
 
     }
 
     flashTiles(total, next){
-      let count = 0;
+      let count = 0, length;
+
       let flash = setInterval( () => {
         if(count === 6) {
           clearInterval(flash);
           return next();
         }
- 
-        for(let i = 1; i < total; i++) {
-          let doc = document.getElementById(i);        
+        
+        for(let i = 0; i < total.length; i++) {
+          let doc = document.getElementById(total[i]);        
           count % 2 ? doc.classList.add('flash') 
                     : doc.classList.remove('flash');
         }
@@ -216,21 +283,25 @@ export class App extends Component{
     
     // handle cpu response to player 
     cpuHandler(record, playedTiles) {
+      
       const [corner, side]   = [[1,3,7,9], [2,4,6,8]],
             [clone, choices] = [[],[]];
-
-      let diagStrategy,
-          offense = true,
+      
+      let forecast = this.board.project
+      
+      let offense = true,
           num     = 0;
-     
+      
       const random = (range) => {
         let milliseconds = new Date().getMilliseconds();
         return Math.floor(milliseconds * range / 1000);
       };
-      
+      console.log('record', record, playedTiles, 'playedTiels')
+      console.log('board' , this.board)
       const checkDefense = (record, player) => {
         const [defense, altDefense, played, unplayed] = [[], [], [], []];
         let threat;
+        console.log('board' , this.board)
         // make a clone of the arr
         arr.map( item => clone.push(item.slice()));
         
@@ -281,86 +352,54 @@ export class App extends Component{
         if(playedTiles < 5) { 
           threat = strategy(played);      
           if(threat) {        
-            console.log('...threat found, ', threat)
             if(!record[threat]) return { results: threat,
-                                        offense: offense,
-                                        type: 'threat'
+                                         offense: offense,
+                                         type: 'threat'
                                         };
           }
         }
-
-        console.log('...no threat found, continue searching...')
-        
-        // access winning combos and returns a possible move.
-        for(var i = 0; i < clone.length; i++) {
-          var count = 0;         
-          for(var j = 0; j < clone[i].length; j++) {           
-            for(var k = 0; k < played.length; k++) {
-              if(clone[i][j] === played[k]) {
-                count++;
-                // for(var l = 0; l < unplayed.length; l++) {
-                //   if(unplayed[i] !== clone[i][j]) {
-                //     monitor[player][i].push(clone[i][j]);
-                //   }
-                // }
-                console.log('recordTiles', record)
-                clone[i].splice(j, 1);
-                
-              }
-            }
-          }    
-
-          if(count===3) return { type: 'win', results: arr[i] };
-
-          if(count===2) {
-            if(!record[clone[i][0]]) { 
-              defense.push(clone[i][0]);
-              clone[i].splice(0, 1);
+       
+        if(forecast['player'] || forecast['computer'] ) {
+          var keys = Object.keys(forecast);
+         
+          for(var i = 0; i < keys.length; i++) {
+            for(var j=0; j < forecast[keys[i]].length; j++ ) {
+              num = forecast[keys[i]][j];
               
-              return {
-                results: defense[0],
-                offense: offense,
-                type: 'strategy',
-                player: player
-              };
-            }  
-            
-            if(i === 6 || i === 7 ) {        
-              for(let i = 2; i < 10; i += 2) {
-                if(!record[i]) altDefense.push(i);
-              }           
-            } 
-          } 
-        }
+              if(typeof num === 'number' ) {
+                if(keys[i] === 'player') {
+                  forecast['block'] = num;
+                }
+                if(keys[i] === 'computer') {
+                  forecast['win'] = num;
+                }
+              }              
+            }
+          }
+        };
 
-        if(altDefense[0]){
-          console.log('...diag strategy found, ', altDefense)
+        console.log('forecast', forecast)
+        if(forecast.win) {
           return {
-            results: altDefense[random(altDefense.length)],
-            offense: offense,
-            type: 'diag strategy',
-            player: player
-          };
+            results : forecast.win,
+            type: 'win',
+          }
         }
-        
-        console.log('...no defense found')
-        if(offense) {
-          offense = false;
-          clone.splice(0);
-          player = (player === 'X') ? 'O' : 'X';
-          console.log('...searching ofensive move')
-          return checkDefense(record, player);
-        } 
 
-        console.log('...no move found, plugging random, ', unplayed)
+        if(forecast.block) {
+          return {
+            results : forecast.block,
+            type: 'block'
+          }
+        }
+
         return {
           results: unplayed[random(unplayed.length)],
           offense: offense,
           type: 'random',
-          player: player
         };
       };
-      console.log('playedTiles', playedTiles, typeof playedTiles)     
+   
       //logical operations for computer to play based on the first few plays
       switch (playedTiles) {
         case 0:
@@ -383,25 +422,17 @@ export class App extends Component{
           break;         
           
         default:
-          num = checkDefense(record, this.state.player); //playedTiles < 7 ? this.state.player : this.state.computer         
+          num = checkDefense(record, this.state.player);         
           break;
       }
 
-      console.log(num)
+      console.log('num', num)
       if(typeof num === 'object') {
         num = num.results;
       }
-      // if(typeof num === 'string') {
-      //   for(let i = 2; i < 10; i += 2) {
-      //     if(!record[i]) choices.push(i);
-      //     num = choices[random(choices.length)];
-      //     if(!num) num = remains[random(remains.length)];
-      //   }
-      // }   
-      console.log('monitor', monitor)      
+         
       document.getElementById(num).innerHTML = this.state.computer;
       this.state.unpauseCpu = false;
-      //console.log(this.state.computer, num)
       this.gameLog(this.state.computer, num)
     }
     
@@ -484,6 +515,136 @@ class ErrorBoundary extends React.Component {
    };
 }; 
 
+
+
+
+
+// const checkDefense = (record, player) => {
+//   const [defense, altDefense, played, unplayed] = [[], [], [], []];
+//   let threat;
+//   // make a clone of the arr
+//   arr.map( item => clone.push(item.slice()));
+  
+//   // look for strategic moves
+//   const strategy = (isAThreat) => {     
+//     //looks for strategic plays that give player advantage    
+//     let num;
+
+//     for (var i = 0; i < isAThreat.length; i++) {
+//       for (var j = isAThreat.length-1; j > 0; j--) {
+//         if (
+//           (isAThreat[i] === 2 && isAThreat[j] === 4) ||
+//           (isAThreat[i] === 2 && isAThreat[j] === 7) ||
+//           (isAThreat[i] === 3 && isAThreat[j] === 4)
+//         ) { 
+//           num = 1; }
+        
+//         if (
+//           (isAThreat[i] === 2 && isAThreat[j] === 6) ||
+//           (isAThreat[i] === 1 && isAThreat[j] === 6) ||
+//           (isAThreat[i] === 2 && isAThreat[j] === 9)
+//         ) { num = 3; }
+              
+//         if (
+//           (isAThreat[i] === 4 && isAThreat[j] === 8) ||
+//           (isAThreat[i] === 1 && isAThreat[j] === 8) ||
+//           (isAThreat[i] === 4 && isAThreat[j] === 9)
+//         ) { num = 7; }
+        
+//         if (
+//           (isAThreat[i] === 6 && isAThreat[j] === 8) ||
+//           (isAThreat[i] === 3 && isAThreat[j] === 8) ||
+//           (isAThreat[i] === 6 && isAThreat[j] === 7)
+//         ) { num = 9; }
+//       }
+//     }
+    
+//     return num;
+//   };  
+  
+//   // create two arrays for moves played and moves remaining
+//   for(let i = 1; i < 10; i++) {
+//     if(record[i] === player) played.push(i);
+//     if(!record[i]) unplayed.push(i);
+//   }
+
+//   // run function to determin if strategy being set up
+//   if(playedTiles < 5) { 
+//     threat = strategy(played);      
+//     if(threat) {        
+//       //console.log('...threat found, ', threat)
+//       if(!record[threat]) return { results: threat,
+//                                    offense: offense,
+//                                    type: 'threat'
+//                                   };
+//     }
+//   }
+
+//   //console.log('...no threat found, continue searching...')
+  
+//   // access winning combos and returns a possible move.
+//   for(var i = 0; i < clone.length; i++) {
+//     var count = 0;         
+//     for(var j = 0; j < clone[i].length; j++) {           
+//       for(var k = 0; k < played.length; k++) {
+//         if(clone[i][j] === played[k]) {
+//           count++;
+//           clone[i].splice(j, 1);
+          
+//         }
+//       }
+//     }    
+
+//     if(count===3) return { type: 'win', results: arr[i] };
+
+//     if(count===2) {
+//       if(!record[clone[i][0]]) { 
+//         defense.push(clone[i][0]);
+//         clone[i].splice(0, 1);
+        
+//         return {
+//           results: defense[0],
+//           offense: offense,
+//           type: 'strategy',
+//           player: player
+//         };
+//       }  
+      
+//       if(i === 6 || i === 7 ) {        
+//         for(let i = 2; i < 10; i += 2) {
+//           if(!record[i]) altDefense.push(i);
+//         }           
+//       } 
+//     } 
+//   }
+
+//   if(altDefense[0]){
+//     //console.log('...diag strategy found, ', altDefense)
+//     return {
+//       results: altDefense[random(altDefense.length)],
+//       offense: offense,
+//       type: 'diag strategy',
+//       player: player
+//     };
+//   }
+  
+//   //console.log('...no defense found')
+//   if(offense) {
+//     offense = false;
+//     clone.splice(0);
+//     player = (player === 'X') ? 'O' : 'X';
+//     //console.log('...searching ofensive move')
+//     return checkDefense(record, player);
+//   } 
+
+//   //console.log('...no move found, plugging random, ', unplayed)
+//   return {
+//     results: unplayed[random(unplayed.length)],
+//     offense: offense,
+//     type: 'random',
+//     player: player
+//   };
+// };
 
 // const flashTiles = (total, next) => {
 //   let count = 0;
